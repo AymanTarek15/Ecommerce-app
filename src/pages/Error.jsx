b8ace8c3f9a55e7f834c97337c974281a0308ca8
unknown <ayman1507879@miuegypt.edu.eg>
2024-07-16 16:24:28 +0300

    Update Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -11,7 +11,7 @@ function Error(){
         Oops! Page not found.
       </div>
       <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
-      <Link href="/" className="btn btn-primary btn-home">Go to Homepage</Link>
+      <Link to={'/'} className="btn btn-primary btn-home">Go to Homepage</Link>
     </div>
   </div>
   ) 
